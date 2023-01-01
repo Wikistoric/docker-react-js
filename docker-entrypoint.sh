2e063345ec7d26eb09dfe0de51974410167ac700
chz <christian@wikistoric.org>
2023-01-01 17:14:11 +0100

    Add feature to install packages via env-variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+cd /node
+if [ -n "$NMPINSTALL" ] ;then
+  npm install $NMPINSTALL
+fi
+
+cd /app
 if [ ! -d /app/src ] ;then
-  cd /app
   (cd /node; tar -c --exclude node_modules -f - *) | tar xf -
+fi
+if [ ! -e /app/node_modules ] ;then
   ln -s ../node/node_modules
+elif [ ! -L /app/node_modules ] ;then
+  echo "/app/node_modules exists and is not a link. The image seems to be broken."
 fi
 
 exec "$@"
